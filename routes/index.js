071a49dbcbafd6bea1614f616b631af972704ce9
adelinger <31591846+adelinger@users.noreply.github.com>
2018-07-25 14:28:42 +0200

    newCommit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index')
 });
+
+router.post('/', function(req, res) {
+    var inputData = request.body.randomsearch
+    response.render('results.ejs',{ data:inputData})
+})
 
 module.exports = router;
