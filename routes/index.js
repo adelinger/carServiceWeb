a15c37daf777b279febc35b93c68e6a9b3a26329
Antun Delinger <antun94@hotmail.com>
2018-08-17 15:23:05 +0200

    general

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index')
 });
+router.get('/main.ejs', function(req, res, next) {
+    res.render('main.ejs')
+})
 
 /*
 router.get('/homePage',isAuthenticated(),function (req, res) {
