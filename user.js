02118e74d4b50fe7038b5856e36db5f73b40d6d1
adelinger <31591846+adelinger@users.noreply.github.com>
2018-08-30 14:11:16 +0200

    finished auth with admin type user

---
+++ user.js
@@ -0,0 +1,14 @@
+class user {
+
+    constructor(name, lastName, email, adress, city, phone, uid) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.adress = adress;
+        this.city = city;
+        this.phone = phone;
+        this.uid = uid;
+    }
+
+}
+
